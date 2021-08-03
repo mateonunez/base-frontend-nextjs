d239919a780d02eecdf13f4cc9b881a5da94a4d3
mateonunez <mateonunez95@gmail.com>
2021-08-03 12:24:13 +0200

    chore: lint and format

--- pages/api/books.js
+++ pages/api/books.js
@@ -1,22 +1,22 @@
 export default function books(req, res) {
   res.setStatusCode = 200;
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader('Content-Type', 'application/json');
 
   return res.json([
     {
       id: 1,
-      book: "The Firm",
-      author: "John Grisham",
+      book: 'The Firm',
+      author: 'John Grisham'
     },
     {
       id: 2,
-      book: "Cracking the PM interview",
-      author: "Jackie Bavaro",
+      book: 'Cracking the PM interview',
+      author: 'Jackie Bavaro'
     },
     {
       id: 3,
-      book: "Fools Die",
-      author: "Mario Puzo",
-    },
+      book: 'Fools Die',
+      author: 'Mario Puzo'
+    }
   ]);
 }

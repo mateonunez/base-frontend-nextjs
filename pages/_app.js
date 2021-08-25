5833eb0be7725ce45a0939c829795581e1e51e1e
mateonunez <mateonunez95@gmail.com>
2021-08-25 11:03:21 +0200

    chore(App): create loading hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { useEffect, useState } from 'react';
+
+import Loading from '../pages/Loading';
+import { useRouter } from 'next/router';
+
+function App({ Component, pageProps }) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Setting isLoading
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+
+    // Changing route
+    router.events.on('routeChangeStart', () => {
+      console.log('Route is changing');
+      setIsLoading(true);
+    });
+
+    // Changed route
+    router.events.on('routeChangeComplete', () => {
+      console.log('Route changed');
+      setIsLoading(false);
+    });
+  }, [router.events]);
+
+  return <>{isLoading ? <Loading /> : <Component {...pageProps} />}</>;
 }
 
-export default MyApp;
+export default App;

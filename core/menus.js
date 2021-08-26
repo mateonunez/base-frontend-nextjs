3e58aa2212d0c4a95c74ab7c7a5eaef9792613b4
mateonunez <mateonunez95@gmail.com>
2021-08-26 10:06:56 +0200

    feat(Footer): add main footer

--- core/menus.js
+++ core/menus.js
@@ -13,4 +13,40 @@ const MainDropdownMenu = [
   }
 ];
 
-export { MainDropdownMenu };
+const MainFooterMenu = [
+  {
+    key: 'about',
+    name: 'About',
+    href: '/about',
+    icon: ''
+  },
+  {
+    key: 'blog',
+    name: 'Blog',
+    href: '/blog',
+    icon: ''
+  }
+];
+
+const ResourcesFooterMenu = [
+  {
+    key: 'terms-conditions',
+    name: 'Terms & Conditions',
+    href: '/terms-conditions',
+    icon: ''
+  },
+  {
+    key: 'privacy-policy',
+    name: 'Privacy Policy',
+    href: '/privacy-policy',
+    icon: ''
+  },
+  {
+    key: 'contact-us',
+    name: 'Contact Us',
+    href: '/contact-us',
+    icon: ''
+  }
+];
+
+export { MainDropdownMenu, MainFooterMenu, ResourcesFooterMenu };
